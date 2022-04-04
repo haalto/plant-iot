d3724fb6a1bc76d3baaa6833a6b27248224a678d
Hannes Aaltonen <aaltonen.hannes@gmail.com>
2022-04-04 19:10:15 +0300

    fix topic subscription

--- services/backend/src/index.ts
+++ services/backend/src/index.ts
@@ -1,3 +1,4 @@
+import { connect } from "mqtt";
 import { app } from "./app";
 import { config } from "./config";
 import { mqttClient, mqttHandler } from "./data-layer/mqtt";
@@ -13,5 +14,45 @@ const { port, host } = config;
     }
   });
 
-  mqttHandler(mqttClient, "iot")();
+  const { mqttUrl, mqttUser, mqttPassword } = config;
+  const topic = "iot";
+
+  const client = connect(mqttUrl, {
+    username: mqttUser,
+    password: mqttPassword,
+    clientId: "foo",
+    clean: false,
+  });
+
+  client.on("connect", () => {
+    client.subscribe(topic, { qos: 1 }, (err, granted) => {
+      if (err) {
+        console.error(err);
+      }
+      console.log(granted.toString());
+      console.log(`Subscribed to ${topic} with QoS ${granted[0].qos}`);
+    });
+  });
+
+  client.on("message", (topic, message, packet) => {
+    console.log(packet, packet.payload.toString());
+    const logMessage = { msg: message.toString(), topic };
+    console.log(JSON.stringify(logMessage));
+    /*     const measurement: NewMeasurement = await JSON.parse(message.toString());
+    try {
+      await createMeasurement(measurement);
+    } catch (e) {
+      console.error(e);
+    } */
+  });
+
+  client.on("error", (err) => {
+    console.log(err);
+  });
+
+  client.on("disconnect", () => {
+    console.log("MQTT client disconnected");
+  });
+
+  //mqttHandler(mqttClient, "iot")();
 })();
