d6889ff13f2d29d17a6cb6bb5dad71cd98a82c42
Hannes Aaltonen <aaltonen.hannes@gmail.com>
2022-04-04 19:06:00 +0300

    fix topic subscription

--- services/backend/src/data-layer/mqtt.ts
+++ services/backend/src/data-layer/mqtt.ts
@@ -8,28 +8,31 @@ const { mqttUrl, mqttUser, mqttPassword } = config;
 export const mqttClient = connect(mqttUrl, {
   username: mqttUser,
   password: mqttPassword,
+  clientId: "foo",
+  clean: false,
 });
 
 export const mqttHandler = (client: MqttClient, topic: string) => () => {
   client.on("connect", () => {
-    client.subscribe(topic, { qos: 1 }, (err) => {
+    client.subscribe(topic, { qos: 1 }, (err, granted) => {
       if (err) {
         console.error(err);
       }
-      console.log(`Subscribed to ${topic}`);
+      console.log(granted.toString());
+      console.log(`Subscribed to ${topic} with QoS ${granted[0].qos}`);
     });
+  });
 
-    client.on("message", (topic, message) => {
-      console.log("foo iot");
-      const logMessage = { msg: message.toString(), topic };
-      console.log(JSON.stringify(logMessage));
-      /*     const measurement: NewMeasurement = await JSON.parse(message.toString());
-      try {
-        await createMeasurement(measurement);
-      } catch (e) {
-        console.error(e);
-      } */
-    });
+  client.on("message", (topic, message, packet) => {
+    console.log(packet, packet.payload.toString());
+    const logMessage = { msg: message.toString(), topic };
+    console.log(JSON.stringify(logMessage));
+    /*     const measurement: NewMeasurement = await JSON.parse(message.toString());
+    try {
+      await createMeasurement(measurement);
+    } catch (e) {
+      console.error(e);
+    } */
   });
 
   client.on("error", (err) => {
