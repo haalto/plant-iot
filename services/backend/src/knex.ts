f62f585f5447b096a3b388d286f875fdb521450c
Hannes Aaltonen <aaltonen.hannes@gmail.com>
2022-04-09 12:52:48 +0300

    fix db connection

--- services/backend/src/knex.ts
+++ services/backend/src/knex.ts
@@ -14,9 +14,12 @@ const DB_CONFIG: Knex.Config = {
   client: "pg",
   connection: {
     connectionString: databaseUrl,
-    ssl: {
-      rejectUnauthorized: false,
-    },
+    ssl:
+      process.env.NODE_ENV === "production"
+        ? {
+            rejectUnauthorized: false,
+          }
+        : false,
   },
 };
 
